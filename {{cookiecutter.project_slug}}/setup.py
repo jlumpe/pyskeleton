b1cb31f43242545d3fbae13918276a82979e632d
Jared Lumpe <mjlumpe@gmail.com>
2018-05-16 20:15:26 -0700

    Don't try to use fancy setup.cfg features

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,39 +1,39 @@
-"""Setuptools installation script for {{ cookiecutter.project_name }} package.
+"""Setuptools installation script for {{ cookiecutter.project_name }} package."""
 
-Most options set in setup.cfg (requires setuptools 30.3 or above).
-"""
-
-from setuptools import setup, __version__ as setuptools_v_str
+from setuptools import setup
 
 
-metadata = dict(
+# Get contents of README file
+with open('README.md') as fh:
+	readme_contents = fh.read()
+
+
+requirements = [
+]
+
+setup_requirements = ['pytest-runner']
+
+test_requirements = ['pytest']
+
+
+setup(
 	name='{{ cookiecutter.project_slug }}',
 	version='{{ cookiecutter.version }}',
 	description='{{ cookiecutter.project_short_description }}',
+	long_description=readme_contents,
 	author='{{ cookiecutter.author }}',
 	author_email='{{ cookiecutter.author_email }}',
 	url='{{ cookiecutter.project_url }}',
-
-	# classifiers='',
+	python_requires='>=3.5',
+	install_requires=requirements,
+	setup_requires=setup_requirements,
+	test_requiremes=test_requirements,
+	include_package_data=True,
 	# license='',
+	# classifiers='',
 	# keywords=[],
 	# platforms=[],
 	# provides=[],
 	# requires=[],
 	# obsoletes=[],
 )
-
-
-# Check setuptools version
-setuptools_v = tuple(setuptools_v_str.split('.')[:2])
-if setuptools_v < ('30', '3'):
-	# Just warn because this isn't the proper way to check at all
-	from warnings import warn
-	warn(
-		"Warning: setuptools version 30.3 is required for installation."
-		" This probably won't work."
-	)
-
-
-# Run setup
-setup(**metadata)
